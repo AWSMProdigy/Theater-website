8bdbcbca1c6296b7bffa46ba343fcf2aba1f3357
Leah Davis <ileahd96@gmail.com>
2021-11-01 14:39:27 -0400

    Working carousel, need to mess with containers

--- public/index.js
+++ public/index.js
@@ -1,25 +1,21 @@
-/*
-    Carousel
-*/
-$('#carousel-example').on('slide.bs.carousel', function (e) {
-    /*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
-    */
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 5;
-    var totalItems = $('.carousel-item').length;
- 
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+$('#movieCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel .carousel-item').each(function(){
+      var minPerSlide = 4;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+              next = $(this).siblings(':first');
             }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
+          
+          next.children(':first-child').clone().appendTo($(this));
         }
-    }
-});
+  });
